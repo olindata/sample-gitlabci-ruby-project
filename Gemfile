125dd1b48d47c3db7a0a146db7f9fc1dde7d88c2
Farley <andrew@neonsurge.com>
2016-04-04 16:31:53 +0200

    Latest changes

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'test-unit'
+gem 'rubocop'
+gem 'rake'
--- tests/test_sample_ruby_project.rb
+++ tests/test_sample_ruby_project.rb
@@ -1,11 +1,9 @@
-require "./lib/sample_ruby_project.rb"
-require "test/unit"
+require './lib/sample_ruby_project.rb'
+require 'test/unit'
 
+# Class comment
 class TestNAME < Test::Unit::TestCase
-
   def test_sample
     assert_equal(4, 2+2)
   end
-
 end
-
